2d58123ca568cb61e00147afee7b019d9f55077c
Jared-Freeman <j99freeman@gmail.com>
2021-10-12 13:42:03 -0700

    Added Q1 Correlation

---
+++ Q1 Correlation/src/main.cpp
@@ -0,0 +1,171 @@
+ /*
+  * @description: 
+  * 
+  *
+  * @author: 
+  * 
+  *
+  */
+  
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <map>
+#include <math.h>
+
+#include "image.h"
+#include "WriteImage.h"
+#include "ReadImage.h"
+#include "Mask.h"
+
+#include "freeman_arg_parse.h" //A small utility I wrote for extracting command line args.
+
+bool FLAG_DEBUG = true;
+
+void PrintHistogram(std::map<int, float> hist);
+void WriteImageToFile(std::string filename, ImageType& img);
+
+int main(int argc, char** argv)
+{
+  //TODO: Print description of process to console (per program)
+
+  //Extract args into vector
+  std::vector<std::string> args;
+  for(int i=1; i < argc; i++)
+  {
+    std::string next_element(argv[i]);  
+    args.push_back(next_element);
+  }
+  
+	//Fill data structures using args
+  std::vector<std::string> imagePaths = ExtractArgs("-in", args);
+  std::vector<std::string> outputPaths = ExtractArgs("-out", args);
+  std::vector<std::string> maskPaths = ExtractArgs("-mask", args);
+  if(outputPaths.size() > 0 && outputPaths[0].length() > 0)
+  {
+    if(outputPaths[0][outputPaths[0].length()-1] != '/')
+    {
+      outputPaths[0] = outputPaths[0] + "/";
+      //std::cout << outputPaths[0] << "\n";
+    }
+  }
+  if(outputPaths.size() > 1)
+  {
+    std::cout << "Can only specify one output path. Output will be saved in the first output directory.\n";
+  }  
+  if(outputPaths.size() < 1)
+  {
+    std::cout << "Please specify an output directory using the \"-out <output_path>\" args.\n";
+    return 0;
+  }  
+  if(maskPaths.size() < 1)
+  {
+    std::cout << "Please specify an image to use as a mask (add the \"-mask <image_path>\" args to command line).\n";
+    return 0;
+  }  
+  if(maskPaths.size() > 1)
+  {
+    std::cout << "Can only specify one mask path per execution. Only first mask will be used.\n";
+  }  
+  
+    
+  //Process each image
+  for(int i=0; i < imagePaths.size(); i++)
+  {
+    std::cout << "_________________________\n";
+    std::cout << "image " << i << ": \"" << imagePaths[i] << "\"\n";
+    
+    ImageType next_image;
+    ImageType out_image;
+    ImageType mask_image;
+    char *cstr = new char[imagePaths[i].length() + 1];
+    strcpy(cstr, imagePaths[i].c_str());
+    std::readImage(cstr, next_image);
+    
+    char *cstr2 = new char[maskPaths[0].length() + 1];
+    strcpy(cstr2, maskPaths[0].c_str());
+    std::readImage(cstr2, mask_image);
+
+    // DO STUFF
+    // ...
+
+    int sz = 5;
+    int** arr = new int*[sz];
+    for(int i=0; i<sz; i++)
+    {
+      arr[i] = new int[sz];
+      for(int j=0; j<sz; j++)
+      {
+        arr[i][j] = 1;
+      }
+    }
+
+    ImageMask test_mask(sz, sz, arr);
+    ImageMask next_mask(mask_image);
+    next_mask.ApplyMask(next_image, out_image);
+
+    for(int i=0; i<sz; i++)
+      delete [] arr[i];
+    delete [] arr;
+
+    // END DO STUFF
+
+    { //Write to file scope
+
+      //determine original file name from path string
+      std::string original_filename = "";
+      for(int l = imagePaths[i].length(); imagePaths[i][l] != '/' && l >= 0; l--)
+      {
+        if(imagePaths[i][l] != '/')
+        {
+          //std::cout << imagePaths[i][l] << std::endl;
+
+          std::string temp;
+          temp += imagePaths[i][l];
+          original_filename.insert(0, temp);
+        }
+        //throw out extension
+        if(imagePaths[i][l] == '.')
+        {
+          original_filename = "";
+        }
+      }      
+
+
+      std::string out_file = outputPaths[0] + original_filename + "_modified.pgm";
+      WriteImageToFile(out_file, out_image);
+
+    } //end Write to file
+
+    delete [] cstr;
+    delete [] cstr2;
+
+    std::cout << "\n";
+  }
+  
+  return 0;
+}
+
+void PrintHistogram(std::map<int, float> hist)
+{
+  for(auto it : hist)
+  {
+    std::cout << it.first << " " << it.second << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+void WriteImageToFile(std::string filename, ImageType& img)
+{
+  std::string out_file = filename;
+  char *cstr = new char[out_file.length() + 1];
+  strcpy(cstr, out_file.c_str());
+
+  std::writeImage(cstr, img);
+
+  std::cout << " * Saved image: " << out_file << "\n";
+
+  delete [] cstr;
+}
